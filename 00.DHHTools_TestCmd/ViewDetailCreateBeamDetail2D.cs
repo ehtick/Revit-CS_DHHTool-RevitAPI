22ba6310b9d8dae0332427892c14f40dbcf93386
DickyDinh <68648708+MrDinhHoa@users.noreply.github.com>
2022-07-15 17:36:26 +0700

    BoSungCode

--- 00.DHHTools_TestCmd/ViewDetailCreateBeamDetail2D.cs
+++ 00.DHHTools_TestCmd/ViewDetailCreateBeamDetail2D.cs
@@ -27,21 +27,14 @@ namespace DHHTools
             UIDocument uidoc = uiapp.ActiveUIDocument;
             Application app = uiapp.Application;
             Document doc = uidoc.Document;
-
-            #region Get Parameter Framing
-            //List<ElementId> selectColumsElements = new List<ElementId>();
-            //Reference refer = uidoc.Selection.PickObject(ObjectType.Face);
-            //Element element = doc.GetElement(refer);
-            //ElementType elementType = doc.GetElement(element.GetTypeId()) as ElementType;
-            //double elementCover = DhhElementUtils.GetElementCover(doc, element);
-            //Parameter bParameter = elementType.LookupParameter("b");
-            //Parameter hParameter = elementType.LookupParameter("h");
-            //double bFeet = bParameter.AsDouble();
-            //double hFeet = hParameter.AsDouble();
             List<double> blist = new List<double>() { 500, 600, 700, 800, 600, 900 };
             List<double> hlist = new List<double>() { 600, 750, 900, 1000, 700, 1200 };
-            List<int> slTop1List = new List<int>() {2, 4, 5, 7, 9, 7};
-            List<int> slBot1List = new List<int>() { 2, 4, 5, 7, 9, 7 };
+            List<int> slTop1List = new List<int>() {5, 4, 5, 7, 6, 9};
+            List<int> slBot1List = new List<int>() {5, 4, 5, 7, 6, 9};
+            List<int> slTop2List = new List<int>() { 2, 0, 0, 0, 3, 5 };
+            List<int> slBot2List = new List<int>() { 0, 0, 0, 0, 2, 2 };
+            List<int> slnhanhdaiList = new List<int>() { 2, 3, 4, 4, 3, 4 };
+
             double bkhung = blist[0];
             double hkhung = hlist[0];
             for (int i =0; i<blist.Count; i++)
@@ -53,7 +46,7 @@ namespace DHHTools
             }
             bkhung = bkhung + 700;
             hkhung = hkhung + 700;
-            #endregion Get Parameter Framing
+           
             FamilySymbol fselement = (FamilySymbol)new FilteredElementCollector(doc)
                .WhereElementIsElementType()
                .OfCategory(BuiltInCategory.OST_DetailComponents)
@@ -70,14 +63,38 @@ namespace DHHTools
                     XYZ insertPointXmm = new XYZ(bkhung * i, 0, 0);
                     xyz = XYZ.Zero.Add(insertPointX);
                     FamilyInstance newinstance = doc.Create.NewFamilyInstance(xyz, fselement, doc.ActiveView);
+
                     Parameter b_khungParameter = newinstance.LookupParameter("b_khung");
                     Parameter h_khungParameter = newinstance.LookupParameter("h_khung");
                     b_khungParameter.Set(DhhUnitUtils.MmToFeet(bkhung));
                     h_khungParameter.Set(DhhUnitUtils.MmToFeet(hkhung));
+
                     Parameter bParameter = newinstance.LookupParameter("b");
                     Parameter hParameter = newinstance.LookupParameter("h");
                     bParameter.Set(DhhUnitUtils.MmToFeet(blist[i]));
                     hParameter.Set(DhhUnitUtils.MmToFeet(hlist[i]));
+
+                    Parameter slTop1Parameter = newinstance.LookupParameter("SL_Top1");
+                    Parameter slBot1Parameter = newinstance.LookupParameter("SL_Bottom1");
+                    slTop1Parameter.Set(slTop1List[i]);
+                    slBot1Parameter.Set(slBot1List[i]);
+
+                    Parameter slTop2Parameter = newinstance.LookupParameter("SL_Top2");
+                    Parameter slBot2Parameter = newinstance.LookupParameter("SL_Bottom2");
+                    slTop2Parameter.Set(slTop2List[i]);
+                    slBot2Parameter.Set(slBot2List[i]);
+
+                    Parameter dai3nhanhdaiParameter = newinstance.LookupParameter("Thep_Dai3Nhanh");
+                    Parameter dai4nhanhdaiParameter = newinstance.LookupParameter("Thep_Dai4Nhanh");
+                    if(slnhanhdaiList[i] == 2){dai3nhanhdaiParameter.Set(0);dai4nhanhdaiParameter.Set(0);}  
+                    else if (slnhanhdaiList[i] ==3){dai3nhanhdaiParameter.Set(1);dai4nhanhdaiParameter.Set(0);}
+                    else if (slnhanhdaiList[i] == 4){dai3nhanhdaiParameter.Set(0);dai4nhanhdaiParameter.Set(1);}
+
+                    Parameter thepgiaParameter = newinstance.LookupParameter("SL_LopThepGia");
+                    if(hlist[i]<700){thepgiaParameter.Set(0);}  
+                    else if (hlist[i] == 700) { thepgiaParameter.Set(1); }
+                    else if (hlist[i]>=700){thepgiaParameter.Set(Math.Ceiling((hlist[i]-700)/350));}
+
                     ReferenceArray referenceArrayX = new ReferenceArray();
                     ReferenceArray referenceArrayY = new ReferenceArray();
                     referenceArrayY.Append(newinstance.GetReferenceByName("Top"));
