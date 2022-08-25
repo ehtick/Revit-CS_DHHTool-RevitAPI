c83a750667b2384c2fc7e88634dde9ec9f90e9f2
DickyDinh <68648708+MrDinhHoa@users.noreply.github.com>
2022-08-25 16:35:55 +0700

    Update

--- 02-01.DHHTools_CreateSectionBeam-Detail2D/02-01.DHHTools_CreateSectionBeam-Detail2DViewModel.cs
+++ 02-01.DHHTools_CreateSectionBeam-Detail2D/02-01.DHHTools_CreateSectionBeam-Detail2DViewModel.cs
@@ -7,6 +7,7 @@ using System.Windows.Forms;
 using Application = Autodesk.Revit.ApplicationServices.Application;
 using System.Collections.ObjectModel;
 using System;
+using System.Collections.Generic;
 // ReSharper disable All
 #endregion
 
@@ -38,7 +39,7 @@ namespace DHHTools
             doc = uidoc.Document;
             UiDoc = uidoc;
             Doc = UiDoc.Document;
-            
+
         }
         #endregion
 
@@ -70,7 +71,7 @@ namespace DHHTools
                     }
                     else
                     {
-                        beamName =  (xlRange.Cells[rowCnt , 2] as Microsoft.Office.Interop.Excel.Range).Value;
+                        beamName = (xlRange.Cells[rowCnt, 2] as Microsoft.Office.Interop.Excel.Range).Value;
                     }
                     string Location = (xlRange.Cells[rowCnt, 3] as Microsoft.Office.Interop.Excel.Range).Value;
                     double b = (xlRange.Cells[rowCnt, 6] as Microsoft.Office.Interop.Excel.Range).Value;
@@ -115,11 +116,11 @@ namespace DHHTools
                     {
                         slThepTrenL1_P1 = (xlRange.Cells[rowCnt, 18] as Microsoft.Office.Interop.Excel.Range).Value;
                         dkThepTrenL1_P1 = (xlRange.Cells[rowCnt, 19] as Microsoft.Office.Interop.Excel.Range).Value;
-                        
+
                         if ((xlRange.Cells[rowCnt, 20] as Microsoft.Office.Interop.Excel.Range).Value == null)
                         { slThepTrenL1_P2 = 0; }
                         else { slThepTrenL1_P2 = (xlRange.Cells[rowCnt, 20] as Microsoft.Office.Interop.Excel.Range).Value; }
-                        
+
                         if ((xlRange.Cells[rowCnt, 21] as Microsoft.Office.Interop.Excel.Range).Value == null)
                         { dkThepTrenL1_P2 = 0; }
                         else { dkThepTrenL1_P2 = (xlRange.Cells[rowCnt, 21] as Microsoft.Office.Interop.Excel.Range).Value; }
@@ -142,12 +143,19 @@ namespace DHHTools
 
                         slThepDuoiL1 = (xlRange.Cells[rowCnt + 1, 18] as Microsoft.Office.Interop.Excel.Range).Value;
                         dkThepDuoiL1 = (xlRange.Cells[rowCnt + 1, 19] as Microsoft.Office.Interop.Excel.Range).Value;
+
+                        if (dkThepTrenL1_P1 == dkThepTrenL1_P2)
+                        {
+                            slThepTrenL1 = slThepTrenL1_P1 + slThepTrenL1_P2;
+
+                        }
+
                     }
                     else if (Location.Contains("CEN") | Location.Contains("NHỊP"))
                     {
                         slThepTrenL1 = (xlRange.Cells[rowCnt - 1, 18] as Microsoft.Office.Interop.Excel.Range).Value;
                         dkThepTrenL1 = (xlRange.Cells[rowCnt - 1, 19] as Microsoft.Office.Interop.Excel.Range).Value;
-                        
+
                         slThepDuoiL1_P1 = (xlRange.Cells[rowCnt, 18] as Microsoft.Office.Interop.Excel.Range).Value;
                         dkThepDuoiL1_P1 = (xlRange.Cells[rowCnt, 19] as Microsoft.Office.Interop.Excel.Range).Value;
 
@@ -221,7 +229,7 @@ namespace DHHTools
                         DkThepDuoiL2 = dkThepDuoiL2,
                         DkThepDai = dkThepDai,
                         SLThepDai = sLThepDai,
-                        KCThepDai = kcThepDai, 
+                        KCThepDai = kcThepDai,
                         DkThepGia = dkThepGia,
                         SLThepGia = sLThepGia,
                     };
@@ -231,8 +239,107 @@ namespace DHHTools
                 xlsworkbook.Close(true, null, null);
                 xlsApp.Quit();
             }
-        } 
+        }
 
+        #endregion
+
+        #region 05. Draw Beam Detail 2D
+        public void DrawBeamDetail2D()
+        {
+            List<double> blist = new List<double>();
+            List<double> hlist = new List<double>() ;
+            List<int> slTop1List = new List<int>() ;
+            List<int> slBot1List = new List<int>() ;
+            List<int> slTop2List = new List<int>() ;
+            List<int> slBot2List = new List<int>() ;
+            List<int> slnhanhdaiList = new List<int>();
+            for (int i=0; i<AllExcelData.Count;i++)
+            {
+
+            }
+
+            double bkhung = blist[0];
+            double hkhung = hlist[0];
+            for (int i = 0; i < blist.Count; i++)
+            {
+                if (bkhung < blist[i])
+                    bkhung = blist[i];
+                if (hkhung < hlist[i])
+                    hkhung = hlist[i];
+            }
+            bkhung = bkhung + 700;
+            hkhung = hkhung + 700;
+
+            FamilySymbol fselement = (FamilySymbol)new FilteredElementCollector(Doc)
+                 .WhereElementIsElementType()
+                 .OfCategory(BuiltInCategory.OST_DetailComponents)
+                 .OfClass(typeof(FamilySymbol))
+                 .Cast<FamilySymbol>()
+                 .FirstOrDefault(s => s.Name.Equals("ICIC_KC_ThepDamV2"));
+            using (Transaction tran = new Transaction(Doc))
+            {
+                tran.Start("Create Detail Beam 2D");
+                for (int i = 0; i < AllExcelData.Count; i++)
+                {
+                    XYZ xyz = XYZ.Zero;
+                    XYZ insertPointX = new XYZ(DhhUnitUtils.MmToFeet(bkhung) * i, 0, 0);
+                    XYZ insertPointXmm = new XYZ(bkhung * i, 0, 0);
+                    xyz = XYZ.Zero.Add(insertPointX);
+                    FamilyInstance newinstance = doc.Create.NewFamilyInstance(xyz, fselement, doc.ActiveView);
+
+                    Parameter b_khungParameter = newinstance.LookupParameter("b_khung");
+                    Parameter h_khungParameter = newinstance.LookupParameter("h_khung");
+                    b_khungParameter.Set(DhhUnitUtils.MmToFeet(bkhung));
+                    h_khungParameter.Set(DhhUnitUtils.MmToFeet(hkhung));
+
+                    Parameter bParameter = newinstance.LookupParameter("b");
+                    Parameter hParameter = newinstance.LookupParameter("h");
+                    bParameter.Set(DhhUnitUtils.MmToFeet(blist[i]));
+                    hParameter.Set(DhhUnitUtils.MmToFeet(hlist[i]));
+
+                    Parameter slTop1Parameter = newinstance.LookupParameter("SL_Top1");
+                    Parameter slBot1Parameter = newinstance.LookupParameter("SL_Bottom1");
+                    slTop1Parameter.Set(slTop1List[i]);
+                    slBot1Parameter.Set(slBot1List[i]);
+
+                    Parameter slTop2Parameter = newinstance.LookupParameter("SL_Top2");
+                    Parameter slBot2Parameter = newinstance.LookupParameter("SL_Bottom2");
+                    slTop2Parameter.Set(slTop2List[i]);
+                    slBot2Parameter.Set(slBot2List[i]);
+
+                    Parameter dai3nhanhdaiParameter = newinstance.LookupParameter("Thep_Dai3Nhanh");
+                    Parameter dai4nhanhdaiParameter = newinstance.LookupParameter("Thep_Dai4Nhanh");
+                    if (slnhanhdaiList[i] == 2) { dai3nhanhdaiParameter.Set(0); dai4nhanhdaiParameter.Set(0); }
+                    else if (slnhanhdaiList[i] == 3) { dai3nhanhdaiParameter.Set(1); dai4nhanhdaiParameter.Set(0); }
+                    else if (slnhanhdaiList[i] == 4) { dai3nhanhdaiParameter.Set(0); dai4nhanhdaiParameter.Set(1); }
+
+                    Parameter thepgiaParameter = newinstance.LookupParameter("SL_LopThepGia");
+                    if (hlist[i] < 700) { thepgiaParameter.Set(0); }
+                    else if (hlist[i] == 700) { thepgiaParameter.Set(1); }
+                    else if (hlist[i] >= 700) { thepgiaParameter.Set(Math.Ceiling((hlist[i] - 700) / 350)); }
+
+                    ReferenceArray referenceArrayX = new ReferenceArray();
+                    ReferenceArray referenceArrayY = new ReferenceArray();
+                    referenceArrayY.Append(newinstance.GetReferenceByName("Top"));
+                    referenceArrayY.Append(newinstance.GetReferenceByName("Bottom"));
+                    referenceArrayX.Append(newinstance.GetReferenceByName("Left"));
+                    referenceArrayX.Append(newinstance.GetReferenceByName("Right"));
+                    Line lineY = Line.CreateUnbound(insertPointX, XYZ.BasisY);
+                    Line lineX = Line.CreateUnbound(insertPointX, XYZ.BasisX);
+                    Dimension dimensionX = doc.Create.NewDimension(doc.ActiveView, lineX, referenceArrayX);
+                    Dimension dimensionY = doc.Create.NewDimension(doc.ActiveView, lineY, referenceArrayY);
+
+                    XYZ translationX = new XYZ(0, -DhhUnitUtils.MmToFeet(hlist[i] / 2 + 150), 0);
+                    XYZ translationXmm = new XYZ(0, -(hlist[i] / 2 + 150), 0);
+                    ElementTransformUtils.MoveElement(doc, dimensionX.Id, translationX);
+
+                    XYZ translationY = new XYZ(DhhUnitUtils.MmToFeet(blist[i] / 2 + 150), 0, 0);
+                    XYZ translationYmm = new XYZ((blist[i] / 2 + 150), 0, 0);
+                    ElementTransformUtils.MoveElement(doc, dimensionY.Id, translationY);
+                }
+                tran.Commit();
+            }
+        }
         #endregion
     }
 }
