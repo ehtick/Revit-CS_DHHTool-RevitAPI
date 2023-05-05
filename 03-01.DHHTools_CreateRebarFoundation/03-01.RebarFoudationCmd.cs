4b987e2eddb65ed20089882c866d16bf60ef8db5
DickyDinh <68648708+MrDinhHoa@users.noreply.github.com>
2023-05-05 17:55:21 +0700

    Update

---
+++ 03-01.DHHTools_CreateRebarFoundation/03-01.RebarFoudationCmd.cs
@@ -0,0 +1,45 @@
+﻿
+#region Namespaces
+
+//using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+//using DHHTools.RebarFoundation;
+
+#endregion
+
+namespace DHHTools.RebarFoundation
+{
+    [Transaction(TransactionMode.Manual)]
+    public class CreateRebarFounationCmd : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData,
+            ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            //Application app = uiapp.Application;
+            Document doc = uidoc.Document;
+            RebarFoundationViewModel FoundationViewModel = new RebarFoundationViewModel(commandData);
+            return Result.Cancelled;
+            #region Lưu lại Transaction
+
+            //using (TransactionGroup transGr = new TransactionGroup(doc))
+            //{
+            //    transGr.Start("Set Elevation at Top of Beams");
+
+            //    RebarFounationViewModel viewModel
+            //        = new RebarFounationViewModel();
+            //    CreateRebarFounationWindow window
+            //        = new CreateRebarFounationWindow(viewModel);
+            //    bool? dialog = window.ShowDialog();
+            //    if (dialog == false) return Result.Cancelled;
+
+            //    transGr.Assimilate();
+            //}
+            //return Result.Succeeded;
+            #endregion
+        }
+    }
+}
---
+++ 03-03.DHHTools_CreateRebarBeam/Lib/FamilySymbolUtils.cs
@@ -0,0 +1,62 @@
+﻿#region Namespaces
+
+using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+
+#endregion
+
+namespace DHHTools
+{
+    public static class FamilySymbolUtils
+    {
+        public static FamilySymbol GetFamilySymbolFraming(Family family, double b, double h)
+        {
+            List<FamilySymbol> allFamilySymbol = family.GetAllFamilySymbol();
+
+            foreach (var familySymbol in allFamilySymbol)
+            {
+                Parameter bParameter = familySymbol.LookupParameter("b");
+                Parameter hParameter = familySymbol.LookupParameter("h");
+                double bvalue = bParameter.AsDouble();
+                double hvalue = hParameter.AsDouble();
+
+                if (bvalue == b && hvalue == h)
+                {
+                    return familySymbol;
+                }
+            }
+
+
+            // làm tròn đến hàng đơn vị, ví dụ: 2995.5 -> 2996
+            double sectionX = Math.Round(DLQUnitUtils.FeetToMm(b), 0);
+            double sectionY = Math.Round(DLQUnitUtils.FeetToMm(h), 0);
+            string name = string.Concat(sectionX, "x", sectionY);
+
+            FamilySymbol result = null;
+            using (Transaction tx = new Transaction(family.Document))
+            {
+                tx.Start("Create new Framing Type");
+                ElementType s1 = allFamilySymbol[0].Duplicate(name);
+                s1.LookupParameter("b").Set(b);
+                s1.LookupParameter("h").Set(h);
+
+                result = s1 as FamilySymbol;
+                tx.Commit();
+            }
+            return result;
+        }
+        public static List<FamilySymbol> GetAllFamilySymbol(this Family family)
+        {
+            List<FamilySymbol> familySymbols = new List<FamilySymbol>();
+
+            foreach (ElementId familySymbolId in family.GetFamilySymbolIds())
+            {
+                FamilySymbol familySymbol = family.Document.GetElement(familySymbolId) as FamilySymbol;
+                familySymbols.Add(familySymbol);
+            }
+
+            return familySymbols;
+        }
+    }
+}
