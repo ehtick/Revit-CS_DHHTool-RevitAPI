a36b6b0ccb5193f9c2a481d00a66fa6278a4df7f
MrDinhHoa <dinhhoanghoa@gmail.com>
2023-11-22 18:16:45 +0700

    Create New Project

---
+++ 02-01.DrawSectionBeam-Detail2D/DrawSectionBeam.cs
@@ -0,0 +1,27 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _02_01_DrawSectionBeam_Detail2D.MVVM.
+using Autodesk.Revit.Attributes;
+
+namespace _02_01_DrawSectionBeam_Detail2D
+{
+    [Transaction(TransactionMode.Manual)]
+    public class DrawSectionBeam : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            return Execute(commandData.Application);
+        }
+
+        public Result Execute(UIApplication application)
+        {
+            var win = new MVVM.View.vMain();
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ 02_01_DrawSectionBeam_Detail2D/DrawSectionBeam.cs
@@ -0,0 +1,28 @@
+﻿using _02_01_DrawSectionBeam_Detail2D.MVVM.View;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_01_DrawSectionBeam_Detail2D
+{
+    [Transaction(TransactionMode.Manual)]
+    public class DrawSectionBeam: IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            return Execute(commandData.Application);
+        }
+
+        public Result Execute(UIApplication application)
+        {
+            var win = new vMain();
+            win.Show();
+            return Result.Succeeded;
+        }
+    }
+}
