16f0276974fd35cf2fd2d3d17953dd82bb3f4105
MrDinhHoa <dinhhoanghoa@gmail.com>
2023-12-25 17:41:50 +0700

    Create Beam Rebar Project

---
+++ 03_03_RebarBeam3D/MVVM/ViewModel/vmMain.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Xaml.Behaviors.Core;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Autodesk.Revit.DB;
+using System.Collections.Generic;
+using Application = Autodesk.Revit.ApplicationServices.Application;
+using System;
+using System.IO;
+using DHHTools.MVVM.View;
+using System.Windows;
+using System.Windows.Controls;
+using System.Linq;
+using Autodesk.Revit.UI;
+using BIMSoftLib.MVVM;
+
+namespace DHHTools.MVVM.ViewModel
+{
+    public class vmMain: PropertyChangedBase
+    {
+        private static vmMain _dcMain = new vmMain();
+        public static vmMain DcMain { get { return _dcMain; } }
+        public static UIApplication RevitApp;
+        public static Application RevitAppService;
+
+        private ActionCommand _cancelRebarBeam;
+        public ICommand CancelRebarBeam
+        {
+            get
+            {
+                if (_cancelRebarBeam == null)
+                {
+                    _cancelRebarBeam = new ActionCommand(PerformCancelRebarBeam);
+                }
+
+                return _cancelRebarBeam;
+            }
+        }
+        private void PerformCancelRebarBeam(object par)
+        {
+            (par as vMain).Close();
+        }
+
+        private ActionCommand _createRebarBeam;
+        public ICommand CreateRebarBeam
+        {
+            get
+            {
+                if (_createRebarBeam == null)
+                {
+                    _createRebarBeam = new ActionCommand(PerformCreateRebarBeam);
+                }
+
+                return _createRebarBeam;
+            }
+        }
+        private void PerformCreateRebarBeam(object par)
+        {
+            (par as vMain).Close();
+        }
+
+        private ActionCommand _selectBeam;
+        public ICommand SelectBeam
+        {
+            get
+            {
+                if (_selectBeam == null)
+                {
+                    _selectBeam = new ActionCommand(PerformSelectBeam);
+                }
+
+                return _createRebarBeam;
+            }
+        }
+        private void PerformSelectBeam(object par)
+        {
+            (par as vMain).Hide();
+            (par as vMain).Show();
+        }
+    }
+}
---
+++ 03_03_RebarBeam3D/RebarBeam3D.cs
@@ -0,0 +1,37 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.Attributes;
+using static DHHTools.MVVM.View.vMain;
+using static DHHTools.MVVM.ViewModel.vmMain;
+using DHHTools.MVVM.View;
+
+namespace DHHTools
+{
+    [Transaction(TransactionMode.Manual)]
+    public class RebarBeam3D: IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uIApplication = commandData.Application;
+            RevitApp = uIApplication;
+            RevitAppService = RevitApp.Application;
+            UIDocument uIDocument = RevitApp.ActiveUIDocument;
+            Document document = uIDocument.Document;
+            using (TransactionGroup transGroup = new TransactionGroup(document))
+            {
+                transGroup.Start("Rebar Beams");
+                vMain win = new vMain();
+                bool? dialog = win.ShowDialog();
+                if (dialog != false)
+                    return Result.Succeeded;
+                transGroup.Commit();
+            }
+            return Result.Succeeded;
+        }
+    }
+}
