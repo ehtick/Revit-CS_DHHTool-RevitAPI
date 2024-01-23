4cdf03d2924d5c138134841402f96f74a7d32203
MrDinhHoa <dinhhoanghoa@gmail.com>
2024-01-23 18:13:35 +0700

    Create Project

---
+++ 03_01_RebarFoundation3D/RebarFoundation3D.cs
@@ -0,0 +1,34 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.Attributes;
+
+namespace _03_01_RebarFoundation3D
+{
+    [Transaction(TransactionMode.Manual)]
+    public class RebarFoundation3D : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uIApplication = commandData.Application;
+            RevitApp = uIApplication;
+            RevitAppService = RevitApp.Application;
+            UIDocument uIDocument = RevitApp.ActiveUIDocument;
+            Document document = uIDocument.Document;
+            using (TransactionGroup transGroup = new TransactionGroup(document))
+            {
+                transGroup.Start("Rebar Beams");
+                vMain win = new vMain();
+                bool? dialog = win.ShowDialog();
+                if (dialog != false)
+                    return Result.Succeeded;
+                transGroup.Commit();
+            }
+            return Result.Succeeded;
+        }
+    }
+}
