e349a840423b73522517a7f87720e1544bed279e
MrDinhHoa <dinhhoanghoa@gmail.com>
2024-06-13 17:42:08 +0700

    Update

---
+++ 00_00_DHHTool_RevitApp/RevitApp.cs
@@ -0,0 +1,30 @@
+﻿using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00_00_DHHTool_RevitApp
+{
+    public class RevitApp : IExternalApplication
+    {
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            try
+            {
+                RevitApp = application;
+            }
+            catch
+            {
+                return Result.Failed;
+            }
+            return Result.Succeeded;
+        }
+    }
+}
