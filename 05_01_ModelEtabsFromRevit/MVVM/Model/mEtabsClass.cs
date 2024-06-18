9a5f65bebdcc9c3d90e3045ea2715abb26f99ee1
MrDinhHoa <dinhhoanghoa@gmail.com>
2024-06-18 21:12:57 +0700

    Create Grid And Level 90%

--- 05_01_ModelEtabsFromRevit/MVVM/Model/mEtabsClass.cs
+++ 05_01_ModelEtabsFromRevit/MVVM/Model/mEtabsClass.cs
@@ -40,8 +40,9 @@ namespace _05_01_ModelEtabsFromRevit.MVVM.Model
         {
             try
             {
-                XYZ originPoint = uIDocument.Selection.PickPoint();
+                XYZ pickPoint = uIDocument.Selection.PickPoint();
                 FilteredElementCollector elements = new FilteredElementCollector(uIDocument.Document);
+                #region Grid
                 List<Grid> grids = elements.OfCategory(BuiltInCategory.OST_Grids).OfClass(typeof(Grid)).Cast<Grid>().ToList();
                 int NumberFields = grids.Count;
                 string[] TableDataGrid = new string[NumberFields * 11];
@@ -53,14 +54,21 @@ namespace _05_01_ModelEtabsFromRevit.MVVM.Model
                     Curve curve = grid.Curve;
                     XYZ origin = (curve as Line).Origin;
                     XYZ direction = (curve as Line).Direction;
-                    //double check = direction.AngleTo(XYZ.BasisX);
                     double check = direction.DotProduct(XYZ.BasisX);
                     if (Math.Round(check) == 0) { tabledata_Draft.Add("X (Cartesian)"); }
                     else { tabledata_Draft.Add("Y (Cartesian)"); }
                     tabledata_Draft.Add(name);
-                    double v = (curve as Line).Distance(originPoint);
-                    double distance = Math.Round(DhhUnitUtils.FeetToMm(v), 0);
-                    tabledata_Draft.Add((distance / 1000).ToString());
+                    XYZ checkPoint = new XYZ(pickPoint.X, pickPoint.Y, 0);
+                    curve.MakeBound(0, 1);
+                    Line line = Line.CreateBound
+                        (
+                            new XYZ(curve.GetEndPoint(0).X, curve.GetEndPoint(0).Y, 0),
+                            new XYZ(curve.GetEndPoint(1).X, curve.GetEndPoint(1).Y, 0)
+                        );
+                    Line XLine = DhhGeometryUtils.LineToXLine(line);
+                    double v = XLine.Distance(checkPoint);
+                    double distance = Math.Round(DhhUnitUtils.FeetToMeter(v), 1);
+                    tabledata_Draft.Add(DhhUnitUtils.FeetToMeter(v).ToString()); // Lưu ý xứ lý số liệu việc làm tròn
                     tabledata_Draft.Add("");
                     tabledata_Draft.Add("");
                     tabledata_Draft.Add("");
@@ -69,52 +77,30 @@ namespace _05_01_ModelEtabsFromRevit.MVVM.Model
                     if (Math.Round(check) == 0) { tabledata_Draft.Add("End"); }
                     else { tabledata_Draft.Add("Start"); }
                     tabledata_Draft.Add("Yes");
+                    //MessageBox.Show($"{name} + {check} + {distance}");
                 }
                 TableDataGrid = tabledata_Draft.ToArray();
                 MessageBox.Show(grids.Count.ToString());
-                #region ETABS
                 MySapModel.InitializeNewModel(eUnits.kN_m_C);
                 MySapModel.File.NewBlank();
                 string GridSysName = "G1";
                 MySapModel.GridSys.SetGridSys(GridSysName, 0, 0, 0);
-
-                ////int NumberTables = 0;
-                ////string[] TableKey = null;
-                ////string[] TableName = null;
-                ////int[] ImportType = null;
-                ////bool[] IsEmpty = null;
-                ////string TableKeyGet = null;
-
-                ////int NumberFields = 0;
-                ////string[] FieldKey = null;
-                ////string[] FieldName = null;
-                ////string[] Description = null;
-                ////string[] UnitsString = null;
-                ////bool[] IsImporttable = null;
-
                 int TableVersion = 0;
                 string[] FieldsKeysIncluded = { "Name", "Grid Line Type", "ID", "Ordinate", "Angle", "X1", "Y1", "X2", "Y2", "Bubble Location", "Visible" };
-                int NumberRecord = 4;
-
+                int NumberRecord = 0;
                 bool FillImportLog = true;
                 int NumFatalErrors = 0;
                 int NumErrorMsgs = 0;
                 int NumWarnMsgs = 0;
                 int NumInfoMsgs = 0;
                 string ImportLog = null;
-                //string[] FieldKeysIncludedLevel = { "Tower", "Name", "Height", "Master Story", "Similar To", "Splice Story", "Color", "GUID" };
-                //string[] TableDataGrid =
-                //{
-                //    "G1", "X (Cartesian)", "A","0","","","","","","End","Yes",
-                //    "G1", "X (Cartesian)", "B","5","","","","","","End","Yes",
-                //    "G1", "Y (Cartesian)", "1","0","","","","","","Start","Yes",
-                //    "G1", "Y (Cartesian)", "2","10","","","","","","Start","Yes",
 
-                //};
+                #endregion
+
+                #region Level
                 FilteredElementCollector elementsLevel = new FilteredElementCollector(uIDocument.Document);
-                List<Level> levels = elements.OfCategory(BuiltInCategory.OST_Levels).OfClass(typeof(Level)).Cast<Level>().ToList();
-                List<Level> levelSort = (List<Level>)levels.OrderBy(x => x.Elevation);
-                int NumberLevel = levelSort.Count;
+                List<Level> levels = elementsLevel.OfCategory(BuiltInCategory.OST_Levels).OfClass(typeof(Level)).Cast<Level>().ToList();
+                List<Level> levelSort = levels.OrderBy(x => x.Elevation).ToList();
                 List<string> StoryNameList = new List<string>();
                 List<double> StoryHeightList = new List<double>();
                 List<bool> IsMaterStoryList = new List<bool>();
@@ -122,7 +108,18 @@ namespace _05_01_ModelEtabsFromRevit.MVVM.Model
                 List<bool> SpliceAboveList = new List<bool>();
                 List<double> SpliceHeightList = new List<double>();
                 List<int> ColorList = new List<int>();
-
+                double firstHeight = 1;
+                for (int i = 0; i < levelSort.Count; i++)
+                {
+                    StoryNameList.Add(levelSort[i].Name);
+                    if (i == 0) { StoryHeightList.Add(firstHeight); }
+                    else { StoryHeightList.Add(DhhUnitUtils.FeetToMeter(levelSort[i].Elevation - levelSort[i - 1].Elevation)); }
+                    IsMaterStoryList.Add(false);
+                    IsSimilarToStoryList.Add("None");
+                    SpliceAboveList.Add(false);
+                    SpliceHeightList.Add(0);
+                    ColorList.Add(0);
+                }
                 string[] StoryName = StoryNameList.ToArray();
                 double[] StoryHeight = StoryHeightList.ToArray();
                 bool[] isMaterStory = IsMaterStoryList.ToArray();
@@ -130,14 +127,11 @@ namespace _05_01_ModelEtabsFromRevit.MVVM.Model
                 bool[] SpliceAbove = SpliceAboveList.ToArray();
                 double[] SpliceHeight = SpliceHeightList.ToArray();
                 int[] Color = ColorList.ToArray();
-
-
+                MySapModel.Story.SetStories_2(DhhUnitUtils.FeetToMeter(levelSort[0].Elevation) - firstHeight, levelSort.Count, ref StoryName, ref StoryHeight, ref isMaterStory, ref IsSimilarToStory, ref SpliceAbove, ref SpliceHeight, ref Color);
                 MySapModel.DatabaseTables.SetTableForEditingArray("Grid Definitions - Grid Lines", ref TableVersion, ref FieldsKeysIncluded, NumberRecord, ref TableDataGrid);
                 MySapModel.DatabaseTables.ApplyEditedTables(FillImportLog, ref NumFatalErrors, ref NumErrorMsgs, ref NumWarnMsgs, ref NumInfoMsgs, ref ImportLog);
-                MySapModel.Story.SetStories_2(-2, 4, ref StoryName, ref StoryHeight, ref isMaterStory, ref IsSimilarToStory, ref SpliceAbove, ref SpliceHeight, ref Color);
                 MySapModel.View.RefreshWindow();
                 MySapModel.View.RefreshView();
-                
                 #endregion
 
             }
